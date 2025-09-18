0acbd2342c1567a2f7713f611f293d6fc4736393
Pranav M <74840262+pranavmal@users.noreply.github.com>
2025-09-18 11:16:00 -0500

    Add files via upload

--- MyWorld.java
+++ MyWorld.java
@@ -6,19 +6,18 @@ public class MyWorld extends World {
     private Dog dog;    
     private Jack jack;
     private Ninja ninja;
-    private int level;
-    private int y;
+    private Ladder l1;
+    
         private String[][] tiles;
-    private int[][] Worlds;
     public MyWorld(){
         setBackground("img/BG/BG.png");
         cat = new Cat();
         addObject(cat,400, 10);
-        level = 0;
-        y = 0;
+        
+     
         // dog = new Dog();
         // addObject(dog, 200, 122);
-        Worlds = new int [3][3];
+
         jack = new Jack();
         addObject(jack, 80, 70);
 
@@ -27,99 +26,17 @@ public class MyWorld extends World {
         tiles = new String[6][8];
         buildWorld();
         
+        
+        l1 = new Ladder();
+        addObject(l1, 256, 325);
     }
     
-    public void NewWorld1()
-    {
-        if(y < Worlds[level].length-1)
-            {
-            if(cat.getX() == 800 - 100)
-            {
-                if(level == 0)
-                {
-                    if(y == 0)
-                    {
-                        addObject(cat,400, 10);
-                        removeObject(jack);
-                        y++;
-                    }
-                    else if(y == 1)
-                    {
-                    }
-                    else if(y == 2)
-                    {
-                    }
-                }
-                else if(level == 1)
-                {
-                    if(y == 0)
-                    {
-                    }
-                    else if(y == 1)
-                    {
-                    }
-                    else if(y == 2)
-                    {
-                    }
-                }
-                else if(level == 2)
-                {
-                    if(y == 0)
-                    {
-                    }
-                    else if(y == 1)
-                    {
-                    }
-                    else if(y == 2)
-                    {
-                    }
-                }
-            }
-        }
-        if(y == Worlds[level].length-1 && level < Worlds.length-1)
-        {
-            level++;
-            y =0;
-            addObject(cat,100, 10);
-            
-        }
-        
-    }
     public void act(){
-        
-        NewWorld1();
-        
+        if (cat.getLives() == 0) {
+            MyMayflower.changeWorld(new YouLose());
+        }
     }
     
-    public void addRandomObjects()
-    {
-        int rand=0;
-        for(int i = 0; i < tiles.length; i++)
-            for(int j = 0; j < tiles[0].length-3; j++)
-            {
-                rand= (int)(Math.random()*9);
-                if(rand<3)
-                {
-                    if(tiles[i][j]==""){
-                        tiles[i][j]+="yarn";
-                        //addObject(new Yarn(), i*128,j*94);
-                    }
-                }
-            }
-        //boolean added = false;
-        //  int row;
-        //int col;
-        /*  while(!added){
-        row = (int) (Math.random()*tiles.length);
-        col = (int) (Math.random()*(tiles[0].length-3));
-        if(tiles[row][col]=="")
-        {
-        addObject (new Jack(), row*128, col*94);
-        tiles[row][col]="jack";
-        added=true;
-        }*/
-        
-    }
     public void buildWorld()
     {  
         for(int i = 0; i < tiles.length; i++)
