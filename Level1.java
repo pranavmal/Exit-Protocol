8cc3847002dd25be2d310a7460e6d43e4d01ba61
Pranav M <74840262+pranavmal@users.noreply.github.com>
2025-09-24 11:42:10 -0500

    Add files via upload

---
+++ Level1.java
@@ -0,0 +1,114 @@
+import mayflower.*;
+/**
+ * Write a description of class Level1 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Level1 extends World
+{
+    // instance variables - replace the example below with your own
+    private Cat cat;
+    private int level;
+    private String[][] tiles;
+    private Jack jack;
+    private int screenIndex;
+    /**
+     * Constructor for objects of class Level1
+     */
+    public Level1()
+    {
+        setBackground("img/BG/BG.png");
+        screenIndex = 0;
+        cat = new Cat();
+        addObject(cat,400, 10);
+        
+        jack = new Jack();
+        addObject(jack, 80, 70);
+        
+        tiles = new String[6][8];
+        
+        initializeCourse();
+        addYarn();
+        buildWorld();
+    }
+    
+    public void act()
+    {
+        if (cat.getLives() == 0)
+        {
+            MyMayflower.changeWorld(new YouLose());
+        }
+        if(cat.getY()-10 <= 0)
+        {
+            nextScreen();
+        }
+    }
+    public void nextScreen()
+    {
+        screenIndex++;
+        if(screenIndex == 1)
+        {
+            cat.setLocation(400,100);
+            jack.setLocation(100, 100);
+            initializeCourse();
+            addYarn();
+            buildWorld();
+        }
+        else if(screenIndex == 2)
+        {
+             cat.setLocation(400,200);
+            jack.setLocation(200,100);
+            initializeCourse();
+            addYarn();
+            buildWorld();
+            level = 2;
+        }
+        else if(level == 2)
+        {
+            MyMayflower.changeWorld(new Level2());
+        }
+        
+    }
+    private void initializeCourse()
+    {
+        for(int i = 0; i < tiles.length; i++)
+            for(int j = 0; j < tiles[0].length; j++)
+                tiles[i][j]="";
+
+        for(int i = 0; i < tiles[0].length; i++)
+            tiles[5][i]+="ground";
+    }
+
+    private void addYarn()
+    {
+        for (int r = 0; r < tiles.length; r++)
+        {
+            for (int c = 0; c < tiles[r].length; c++)
+            {
+                int rand = 0;
+                rand = (int) (Math.random() * 9) + 1;
+                if (rand <= 2 && tiles[r][c].equals(""))
+                {
+                    tiles[r][c] = "Yarn";
+                }
+            }
+        }
+    }
+
+    public void buildWorld()
+    {  
+
+        for (int r = 0; r < tiles.length; r++)
+        {
+            for (int c = 0; c < tiles[r].length; c++)
+            {
+                if(tiles[r][c].equals("ground"))
+                    addObject (new Block(), c*128, r*95);
+                if (tiles[r][c].equals("Yarn"))
+                    addObject(new Yarn(), c * 128, r * 95);
+
+            }
+        }
+    }
+}
--- MyWorld.java
+++ MyWorld.java
@@ -7,6 +7,7 @@ public class MyWorld extends World {
     private Dog dog;    
     private Jack jack;
     private Ninja ninja;
+    private int level;
     private Ladder l1;
     private ArrayList<Integer> ladderX;
     private ArrayList<Integer> ladderY;
@@ -29,6 +30,8 @@ public class MyWorld extends World {
         //ninja = new Ninja();
         //addObject(ninja, 320, 86);
         tiles = new String[6][8];
+        initializeGround();
+        addYarn();
         buildWorld();
         
         
@@ -50,19 +53,105 @@ public class MyWorld extends World {
         }
     }
     
-    public void buildWorld()
-    {  
+    private void initializeGround()
+    {
         for(int i = 0; i < tiles.length; i++)
             for(int j = 0; j < tiles[0].length; j++)
                 tiles[i][j]="";
 
         for(int i = 0; i < tiles[0].length; i++)
             tiles[5][i]+="ground";
+    }
+    
+    private void addYarn()
+    {
 
-        for(int i = 0; i < tiles.length; i++)
-            for(int j = 0; j < tiles[0].length; j++)
-                if(tiles[i][j].equals("ground"))
-                    addObject (new Block(), j*128, i*95);
+        if (level == 1)
+            for (int r = 0; r < tiles.length; r++)
+            {
+                for (int c = 0; c < tiles[r].length; c++)
+                {
+                    int rand = 0;
+                    rand = (int) (Math.random() * 9) + 1;
+                    if (rand <= 2 && tiles[r][c].equals(""))
+                    {
+                        tiles[r][c] = "Yarn";
+                    }
+                }
+            }
+        else if (level == 2)
+        {
+            for (int r = 0; r < tiles.length; r++)
+            {
+                for (int c = 0; c < tiles[r].length; c++)
+                {
+                    int rand = 0;
+                    rand = (int) (Math.random() * 9) + 1;
+                    if (rand <= 3 && tiles[r][c].equals(""))
+                    {
+                        tiles[r][c] = "Yarn";
+                    }
+                }
+            }
+        }
+        else if (level == 3)
+        {
+            for (int r = 0; r < tiles.length; r++)
+            {
+                for (int c = 0; c < tiles[r].length; c++)
+                {
+                    int rand = 0;
+                    rand = (int) (Math.random() * 9) + 1;
+                    if (rand <= 4 && tiles[r][c].equals(""))
+                    {
+                        tiles[r][c] = "Yarn";
+                    }
+                }
+            }
+        }
+        //boolean added = false;
+        //  int row;
+        //int col;
+        /*  while(!added){
+        row = (int) (Math.random()*tiles.length);
+        col = (int) (Math.random()*(tiles[0].length-3));
+        if(tiles[row][col]=="")
+        {
+        addObject (new Jack(), row*128, col*94);
+        tiles[row][col]="jack";
+        added=true;
+        }*/
+    }
+    
+    // public void buildWorld()
+    // {  
+        // for(int i = 0; i < tiles.length; i++)
+            // for(int j = 0; j < tiles[0].length; j++)
+                // tiles[i][j]="";
 
+        // for(int i = 0; i < tiles[0].length; i++)
+            // tiles[5][i]+="ground";
+
+        // for(int i = 0; i < tiles.length; i++)
+            // for(int j = 0; j < tiles[0].length; j++)
+                // if(tiles[i][j].equals("ground"))
+                    // addObject (new Block(), j*128, i*95);
+
+    // }
+    
+    public void buildWorld()
+    {  
+
+        for (int r = 0; r < tiles.length; r++)
+        {
+            for (int c = 0; c < tiles[r].length; c++)
+            {
+                if(tiles[r][c].equals("ground"))
+                    addObject (new Block(), c*128, r*95);
+                if (tiles[r][c].equals("Yarn"))
+                    addObject(new Yarn(), c * 128, r * 95);
+
+            }
+        }
     }
 }
